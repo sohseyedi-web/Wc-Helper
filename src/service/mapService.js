899d30c90b3c44c8956256c3e6ac35d892bb6310
sohseyedi <sohseyedi@gmail.com>
2024-11-03 11:40:59 +0330

    Implement reverse geocoding API call to fetch location details based on latitude and longitude

---
+++ src/service/mapService.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const reverseGeoApi = async (lat, lng) => {
+
+    const { data } = await axios.get(
+        `https://api.neshan.org/v5/reverse?lat=${lat}&lng=${lng}`,
+        {
+            headers: {
+                "Api-Key": import.meta.env.VITE_API_KEY_SERVICE,
+                "Content-Type": "application/json",
+            },
+        }
+    );
+
+    return data;
+};
