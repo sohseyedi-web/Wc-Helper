4b24eab2c95ae2e557df42c9277821587714cdb2
sohseyedi <sohseyedi@gmail.com>
2024-11-04 10:07:57 +0330

    alert functionality for welcoming users on first visit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { useEffect } from "react";
 import Location from "./components/location/Location"
+import { getCookie, setCookie } from "./utils/cookies";
+import AlertNotice from "./components/AlertNotice";
 
 function App() {
 
+  useEffect(() => {
+    const hasSeenAlert = getCookie('hasSeenAlert');
+
+    if (!hasSeenAlert) {
+      alert('لطفا یادت نره از فیلترشکن استفاده کنی');
+      setCookie('hasSeenAlert', 'true', 365);
+    }
+  }, []);
+
+
   return (
-    <section className="max-w-4xl container mx-auto px-2 lg:px-0">
+    <section className="max-w-4xl container mx-auto px-5 lg:px-0">
       <Location />
+      <AlertNotice />
     </section>
   )
 }
