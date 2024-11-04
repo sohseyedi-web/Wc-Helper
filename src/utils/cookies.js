11af57a3f89a3faebc3ca797451203da0b421288
sohseyedi <sohseyedi@gmail.com>
2024-11-04 10:07:16 +0330

    utility functions to set and get cookies with expiration support

---
+++ src/utils/cookies.js
@@ -0,0 +1,16 @@
+export const setCookie = (name, value, days) => {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    document.cookie = `${name}=${value}; expires=${date.toUTCString()}; path=/`;
+};
+
+export const getCookie = (name) => {
+    const nameEQ = `${name}=`;
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i];
+        while (cookie.charAt(0) === ' ') cookie = cookie.substring(1);
+        if (cookie.indexOf(nameEQ) === 0) return cookie.substring(nameEQ.length, cookie.length);
+    }
+    return null;
+};
